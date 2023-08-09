7935d70917db3a4bd59c5609f5d9515bd106b66b
Noah Smith <noahcsmith1993@gmail.com>
2023-08-09 14:17:32 -0400

    Testing image path changes

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ const messageEl = document.getElementById("message")
 const controlsEl = document.getElementById("controls")
 const buttonsEl = document.getElementById("difficulty")
 const mineImg = document.createElement("img")
-mineImg.src = "/MineSweeperGame/Images/Mine.png"
+mineImg.src = "Images/Mine.png"
 // mineImg.style.display = "none"
 const flagImg = document.createElement("img")
-flagImg.src = "/MineSweeperGame/Images/Flag.png"
+flagImg.src = "Images/Flag.png"
 
 /* ---- Functions ----- */
 
